965cefd9dae355066a94daff400bd646390eefb2
James McGeachie <jamesmcgeachie10@gmail.com>
2015-10-27 15:03:22 -0400

    completed exercise3

---
+++ exercise3.rb
@@ -0,0 +1,6 @@
+puts "What is your name?"
+name = gets.chomp.to_s.capitalize
+puts "Hi #{name}!"
+puts "How old are you? If you don't mind..."
+age = gets.chomp.to_i
+puts "I see, so #{name} is #{age} years old!"
